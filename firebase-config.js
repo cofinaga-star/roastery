c31563c7ec3531496b61f31e3487047915f834c7
tetscoffee <cofinaga@gmail.com>
2026-03-21 01:19:53 +0530

    from oss
    from oss

--- firebase-config.js
+++ firebase-config.js
@@ -1,12 +1,15 @@
+// firebase-config.js
+// ---- DO NOT edit any line except the values copy‑pasted from the console ----
 const firebaseConfig = {
-  apiKey: "AIzaSy...",
-  authDomain: "tet-s-coffee-roastery.firebaseapp.com",
-  projectId: "tet-s-coffee-roastery",
-  storageBucket: "tet-s-coffee-roastery.firebasestorage.app",
-  messagingSenderId: "68019552831",
-  appId: "1:68019552831:web:c6111a7f30b3c16156ae8a",
-  measurementId: "G-CR4EKB2RB5"
+    apiKey:            "AIzaSyB3XJrZRNxx7tvH0ugd49k_kEOUhxp2R60",
+    authDomain:        "tet-s-coffee-roastery.firebaseapp.com",
+    projectId:         "tet-s-coffee-roastery",
+    // ✅ **Important** – the bucket must end with ".appspot.com"
+    storageBucket:    "tet-s-coffee-roastery.appspot.com",
+    messagingSenderId:"68019552831",
+    appId:             "1:68019552831:web:c6111a7f30b3c16156ae8a",
+    // measurementId is optional – it does not affect auth
+    measurementId:    "G-CR4EKB2RB5"
 };
-
-firebase.initializeApp(firebaseConfig);
-
+// Export for the ES‑module import used in app.js
+export default firebaseConfig;
